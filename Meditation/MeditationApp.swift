e37e1e8eb9a05899ebf563e5f8041b3007f74b1a
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-05-05 18:13:01 +0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Ali Mert Özhayta on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
